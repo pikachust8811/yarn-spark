4b02fe2af536eac69cf2342d37cee24de3e0f6a5
ire7715 <ire7715@hotmail.com>
2017-10-31 14:38:26 +0800

    enable yarn

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,13 +11,13 @@ cd $HADOOP_PREFIX/share/hadoop/common ; for cp in ${ACP//,/ }; do  echo == $cp; 
 
 if [ "$NODE_TYPE" == "master" ]; then
   $HADOOP_PREFIX/sbin/hadoop-daemon.sh start namenode
-  # $HADOOP_PREFIX/sbin/yarn-daemon.sh start resourcemanager
+  $HADOOP_PREFIX/sbin/yarn-daemon.sh start resourcemanager
 
   # $HADOOP_PREFIX/bin/hadoop fs -mkdir -p /user/spark/events
   # $SPARK_HOME/sbin/start-history-server.sh
 else
   $HADOOP_PREFIX/sbin/hadoop-daemon.sh start datanode
-  # $HADOOP_PREFIX/sbin/yarn-daemon.sh start nodemanager
+  $HADOOP_PREFIX/sbin/yarn-daemon.sh start nodemanager
 fi
 
 if [[ $1 == "-d" ]]; then
